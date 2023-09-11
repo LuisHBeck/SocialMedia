237634403991a8709bf49693aeffa7f4da11eaa4
Beck <125098484+LuisHBeck@users.noreply.github.com>
2023-09-11 19:24:56 +0000

    feat: getPosts

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../../config/firebase";
+
 export const Home = () => {
-  return (
-    <div>
-      Home Page
-    </div>
-  )
-}
+	const [postList, setPostList] = useState(null);
+	const postRef = collection(db, "posts");
+
+	const getPosts = async () => {
+		const data = await getDocs(postRef);
+    console.log(data);
+	};
+
+  getPosts();
+
+	return <div>Home Page</div>;
+};
