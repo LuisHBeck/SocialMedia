f99108cf8da7c71b2c406c2b93d63a84c13791a8
LuisHBeck <beck.henrique.luis@gmail.com>
2023-09-12 11:30:55 -0300

    feat: posts like

--- src/pages/Home/post.tsx
+++ src/pages/Home/post.tsx
@@ -1,26 +1,110 @@
-import { Post as IPost} from ".";
+import {
+	collection,
+	addDoc,
+	query,
+	where,
+	getDocs,
+	doc,
+  deleteDoc,
+} from "firebase/firestore";
+
+import { Post as IPost } from ".";
+
+import { auth, db } from "../../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useEffect, useState } from "react";
 
 interface Props {
-  post: IPost;
+	post: IPost;
+}
+
+interface Like {
+	userId: string;
 }
 
 export const Post = (props: Props) => {
 	const { post } = props;
 
+	const [user] = useAuthState(auth);
+
+	const [likes, setLikes] = useState<Like[] | null>(null);
+
+	const likesRef = collection(db, "likes");
+	const likesDoc = query(likesRef, where("postId", "==", post.id));
+
+	const getLikes = async () => {
+		const data = await getDocs(likesDoc);
+		setLikes(
+			data.docs.map((doc) => ({ userId: doc.data().userId, id: doc.id }))
+		);
+	};
+
+	const hasLiked = likes?.find((like) => {
+		return like.userId === user?.uid;
+	});
+
+	useEffect(() => {
+		getLikes();
+	}, []);
+
+	const addLike = async () => {
+		try {
+			await addDoc(likesRef, {
+				userId: user?.uid,
+				postId: post.id,
+			});
+			if (user) {
+				setLikes((prev) =>
+					prev ? [...prev, { userId: user?.uid }] : [{ userId: user?.uid }]
+				);
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	const removeLike = async () => {
+		try {
+			const likeToDeleteQuery = query(
+				likesRef,
+				where("postId", "==", post.id),
+				where("userId", "==", user?.uid)
+			);
+
+			const likeToDeleteData = await getDocs(likeToDeleteQuery);
+
+			const likeToDelete = doc(db, "likes", likeToDeleteData.docs[0].id);
+
+			await deleteDoc(likeToDelete);
+
+			// if (user) {
+			// 	setLikes((prev) =>
+			// 		prev ? [...prev, { userId: user?.uid }] : [{ userId: user?.uid }]
+			// 	);
+			// }
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
 	return (
 		<div key={post.id}>
 			<div className="title">
-        <h1> {post.title} </h1>
-      </div>
+				<h1> {post.title} </h1>
+			</div>
 
-      <div className="body">
-        <p> {post.description} </p>
-      </div>
+			<div className="body">
+				<p> {post.description} </p>
+			</div>
 
-      <div className="footer">
-        <p> @{post.username} </p>
-        <button> &#128077; </button>
-      </div>
+			<div className="footer">
+				<p> @{post.username} </p>
+				<button onClick={hasLiked ? removeLike : addLike}>
+					{" "}
+					{hasLiked ? <>&#128078;</> : <>&#128077;</>}{" "}
+				</button>
+				{likes && <p> Likes: {likes?.length} </p>}
+			</div>
 		</div>
 	);
 };
