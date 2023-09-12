d048513ea902a11c315acbea282bf6b7ffd78a1b
LuisHBeck <beck.henrique.luis@gmail.com>
2023-09-11 22:17:25 -0300

    feat: return posts

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { getDocs, collection } from "firebase/firestore";
 import { db } from "../../config/firebase";
 
+import { Post } from "./post";
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export interface Post {
+	id: string;
+	userId: string;
+	username: string;
+	title: string;
+	description: string;
+}
+
 export const Home = () => {
-	const [postList, setPostList] = useState(null);
+	const [postList, setPostList] = useState<Post[] | null>(null);
 	const postRef = collection(db, "posts");
 
 	const getPosts = async () => {
 		const data = await getDocs(postRef);
-    console.log(data);
+		setPostList(
+			data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as Post[]
+		);
 	};
 
-  getPosts();
+	// getPosts();
 
-	return <div>Home Page</div>;
+	useEffect(() => {
+		getPosts();
+	}, []);
+
+	return (
+		<div>
+			{postList?.map((post) => {
+				return (
+					<Post post={post}/>
+				);
+			})}
+		</div>
+	);
 };
